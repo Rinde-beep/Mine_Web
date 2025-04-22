d4efd9c98b451cd058f91f1458b5bb8e6d91b82b
BelgUstas <118297127+belgustas@users.noreply.github.com>
2025-04-23 00:00:45 +0200

    бага нет, регистрация

--- database.py
+++ database.py
@@ -14,13 +14,14 @@ class Base(DeclarativeBase):
 class Users(Base):
     __tablename__ = "user"
 
-
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     user: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str] = mapped_column()
 
+
 def create_db():
     Users.metadata.create_all(engine)
+
 
 def insert_orm_user(name, password):
     with session_factory() as ses:
@@ -28,16 +29,17 @@ def insert_orm_user(name, password):
         ses.add(user)
         ses.commit()
 
+
 def select_orm(name, what):
     with engine.connect() as conn:
-        check = conn.execute(select(what)).where(Users.user == name).scalar()
+        check = conn.execute(select(what).where(Users.user == name)).scalar()
     return check
+
 
 def select_orm_equal(id, what):
     with engine.connect() as conn:
-        check = conn.execute(select(what)).where(Users.id == id).scalar()
+        check = conn.execute(select(what).where(Users.id == id)).scalar()
     return check
-
 
 
 if __name__ == "__main__":
